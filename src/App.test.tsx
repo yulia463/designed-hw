66f50cc436f7ddc8c8e6dc33bf81ce8c08973191
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-02-02 22:25:37 +0100

    feat: update page name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-//test!
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);

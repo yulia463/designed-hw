3c530c12b38ab42476c957f424810146cb18f2b0
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-02-02 17:01:55 +0100

    feat: initial commit

---
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import Message from './message/Message'
+import MessageSender from './message-sender/MessageSender'
+import s2 from '../../s1-main/App.module.css'
+import FriendMessage from './friend-message/FriendMessage'
+import avatar from './avatar.png'
+
+export type MessageType = {
+    id: number
+    user: {
+        avatar: string,
+        name: string
+    }
+        message: {
+            text: string,
+            time: string,
+        }
+}
+/*
+* 1 - описать тип MessageType
+* 2 - описать тип MessagePropsType в файле Message.tsx
+* 3 - в файле Message.tsx отобразить приходящие данные
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
+* 5 - сделать стили в соответствии с дизайном
+* */
+
+// нужно создать правильный тип вместо any
+
+
+// структуру объекта не менять
+export const message0: MessageType = {
+    id: 0,
+    user: {
+        avatar: avatar, // можно менять
+        name: 'Ivan',  // можно менять
+    },
+    message: {
+        text: 'Hello, she didn’t do anything and rested all day, how are you?', // можно менять
+        time: '09:01', // можно менять
+    },
+}
+export const friendMessage0: MessageType = {
+    id: 100,
+    user: {
+        avatar: avatar, // можно менять
+        name: 'Ivan', // можно менять
+    },
+    message: {
+        text: 'Hello, how are you, what did you do yesterday?', // можно менять
+        time: '09:00', // можно менять
+    },
+}
+
+const HW1 = () => {
+    return (
+        <div id={'hw1'}>
+            <div className={s2.divider}></div>
+            <div className={s2.hwTitle}>Homework #1</div>
+            <div className={s2.divider}></div>
+            <div className={s2.hw}>
+                {/*проверка отображения (не менять)*/}
+                <div style={{display: 'flex' , flexDirection:'column'}}>
+                    <Message message={message0}/>
+                    <FriendMessage message={friendMessage0}/>
+                </div>
+
+                {/*для автоматической проверки дз (не менять)*/}
+                <MessageSender M={Message}/>
+            </div>
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import s from './Message.module.css'
+import {MessageType} from "../HW1";
+
+// нужно создать правильный тип вместо any
+export type MessagePropsType = {
+    message:MessageType
+}
+
+// нужно отобразить приходящие данные
+const Message = (props: MessagePropsType) => {
+    return (
+        <div id={'hw1-message-' + props.message.id} className={s.message}>
+            <div className={s.imageAndText}>
+                <div className={s.text}>
+                    <div id={'hw1-name-' + props.message.id} className={s.name}>
+                        {props.message.user.name}
+                    </div>
+                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+                        {props.message.message.text}
+                    </pre>
+                </div>
+                <div className={s.triangle}></div>
+                <img
+                    src={props.message.user.avatar}
+                    id={'hw1-avatar-' + props.message.id}
+                />
+            </div>
+            <div id={'hw1-time-' + props.message.id} className={s.time}>
+                {props.message.message.time}
+            </div>
+        </div>
+    )
+}
+
+export default Message

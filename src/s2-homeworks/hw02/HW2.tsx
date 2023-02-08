c86cc0149cfa9b12076dde59872f06182c97ba4c
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-02-08 14:13:18 +0100

    feat: homework 2

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -59,7 +59,7 @@ function HW2() {
 
     return (
         <div id={'hw2'}>
-            <div className={s2.hwTitle}>Homework #2</div>
+            <div className={`${s2.hwTitle} ${s2.divider}`}>Homework #2</div>
             <div className={s2.hw}>
                 <Affairs
                     data={filteredAffairs}

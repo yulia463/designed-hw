a89ecc24a2e9f89617f2998152f14f1a23c70552
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-03-11 14:48:01 +0100

    feat: homework 6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,8 +18,8 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
-
+        setValue(restoreState('hw6-editable-span-value',value)
+        )
     }
 
     return (

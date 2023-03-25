9550ee4613095d325152aef503700722817cef1e
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-03-25 13:13:03 +0100

    feat: homework 8

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HW6 from "../../hw06/HW6";
 import HW7 from "../../hw07/HW7";
+import HW8 from "../../hw08/HW8";
 // import HW6 from '../../hw06/HW6'
 // import HW7 from '../../hw07/HW7'
 // import HW8 from '../../hw08/HW8'
@@ -11,7 +12,7 @@ function Junior() {
         <div id={'hw5-page-junior'}>
             <HW6 />
             <HW7 />
-            {/*<HW8 />*/}
+            <HW8 />
             {/*<HW9 />*/}
         </div>
     )
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,39 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            let stateCopy = [...state]
 
-            return state // need to fix
+            return action.payload === 'up'
+                ? stateCopy.sort((a, b) => {
+
+                    if (a.name < b.name) {
+                        return -1
+                    }
+                    if (a.name > b.name) {
+                        return 1
+                    }
+                    return 0
+                })
+                : stateCopy.sort((a, b) => {
+
+                    if (a.name > b.name) {
+                        return -1
+                    }
+                    if (a.name < b.name) {
+                        return 1
+                    }
+                    return 0
+                })
+
+
+            return stateCopy // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state

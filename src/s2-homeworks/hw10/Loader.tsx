c5912b78a4429f76073a2560be1ac289699bee8d
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-04-11 16:56:41 +0200

    hw 10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,11 @@
 import s from './Loader.module.css'
+import preloader from './bll/preloader.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () =>{
+    return (
+      //  <div className={s.loader}/>
+        <img src={preloader} alt={'preloader'} className={s.loader}/>
+    )
+}
+
+
